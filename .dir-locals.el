aad9233f39ed1f942f963013604e65f27322f8cd
jiacai2050 <jiacai2050@gmail.com>
2021-07-03 11:13:06 +0800

    update about, add github workflows

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,16 +1,12 @@
-((org-mode . ((eval .
-                    (progn
-                      (setq-local org-download-image-dir (expand-file-name "./static/images" (file-name-directory buffer-file-name))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
 
-                                  org-download-link-format-function (lambda (filename)
-                                                                      (format "[[/images/%s]]" (file-name-nondirectory filename))))
-                      ;; (make-local-variable 'org-link-parameters)
-                      ;; (org-link-set-parameters
-                      ;;  "file"
-                      ;;  :follow (lambda (path)
-                      ;;            (let (newpath (expand-file-name path "/tmp"))
-                      ;;              (message "newpath %s" newpath))
-                      ;;            newpath
-		              ;;            ))
-                      )
-                    ))))
+((org-mode . ((eval . (progn
+                        (setq-local org-download-image-dir
+                                    (expand-file-name "./static/images"
+                                                      (file-name-directory buffer-file-name))
+                                    org-download-link-format-function
+                                    (lambda
+                                      (filename)
+                                      (format "[[/images/%s]]"
+                                              (file-name-nondirectory filename)))))))))
